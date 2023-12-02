a2a1b6b503ad43514bb23b8996eefc2aa78ddc4f
Jingwen Zhang <wz1421@ic.ac.uk>
2023-12-02 15:01:03 +0000

    'Added warning messages to the sign up page'

---
+++ Website/models.py
@@ -0,0 +1,19 @@
+#creating data base models for users and baby data
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    #unqiue id
+    id = db.Column(db.Integer, primary_key = True)
+    data = db.Column(db.String(10000))
+    #gets the current date and time
+    #can use it to store the baby data
+    date = db.Column(db.DateTime(timezone=True), default =func.now())
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    #maximum string of email is 150
+    email = db.Column(db.String(150),unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
